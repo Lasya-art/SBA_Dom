9e3a944236030ff74ef0ad049e8ce89ac1a1fd06
Lasya-art <lasyagwd@gmail.com>
2024-10-21 21:19:26 -0400

    first commit

--- index.js
+++ index.js
@@ -36,6 +36,9 @@ mainContent.style.backgroundColor = 'var(--main-bg)';
 const h1 = document.createElement('h1');
 h1.textContent = 'Hello, !';
 mainContent.appendChild(h1);
+const h3 = document.createElement('h3');
+h3.textContent = 'Welcome to Villages Commiunity';
+mainContent.appendChild(h3);
 const topMenuEl = document.getElementById("top-menu");
 const subMenuEl = document.getElementById("sub-menu");
 topMenuEl.style.height = "100%";
@@ -49,6 +52,14 @@ const firstnameInput = document.getElementById("firstname");
 const lastnameInput = document.getElementById("lastname");
 const emailInput = document.getElementById("email");
 const phoneInput = document.getElementById("phone");
+
+    form.style.backgroundColor = "#573737";
+    form.style.padding = "20px";
+    form.style.color="#ccc";
+    
+    form.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.1)";
+    form.style.width = '300px';
+  
 
 
 
@@ -109,7 +120,7 @@ for (const link of menuLinks) {
 function hideSubMenuOnClickOutside(event) {
 
     if (!topMenuEl.contains(event.target) && !subMenuEl.contains(event.target)) {
-        subMenuEl.style.display = "none"; // 
+        subMenuEl.style.display = "none"; 
     }
 }
 
@@ -119,11 +130,48 @@ document.addEventListener("click", hideSubMenuOnClickOutside);
 registerButton.addEventListener('click', function () {
     event.preventDefault();
 
+    
+    //const firstname = firstnameInput.value;
     const firstname = document.querySelector('input[name="Firstname"]').value;
     const lastname = document.querySelector('input[name="Lastname"]').value;
     const email = document.querySelector('input[name="email"]').value;
     const phone = document.querySelector('input[name="phone"]').value;
     const date = document.querySelector('input[name="date"]').value;
+
+        
+        if (firstname === "") {
+            alert("Please enter your first name.");
+            document.querySelector('input[name="Firstname"]').focus();
+            return;
+        }
+
+
+
+    if (lastname === "") {
+        alert("Please enter your last name.");
+        lastname.focus(); 
+        return; 
+    }
+
+    if (email === "") {
+        alert("Please enter your email address."); 
+        document.querySelector('input[name="email"]').focus(); 
+        return; 
+    }
+
+    if (phone === "") {
+        alert("Please enter your phone number."); 
+        document.querySelector('input[name="phone"]').focus(); 
+        return; 
+
+    }
+
+    if (date === "") {
+        alert("Please enter your move-in date."); 
+        document.querySelector('input[name="date"]').focus(); 
+        return; 
+    }
+
 
    
     window.alert(`Thank you for booking a tour, ${firstname} ${lastname}`);
